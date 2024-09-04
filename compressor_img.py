6cf27f79bc67a8a5fcc857274d826fe1979821c0
Reyhan Andrea Firdaus <77052916+ReyhanAnf@users.noreply.github.com>
2024-09-05 00:20:59 +0700

    Update compressor_img.py

--- compressor_img.py
+++ compressor_img.py
@@ -3,10 +3,10 @@ from PIL import Image
 
 def cpic(file, outname, i='only'):
 	try:
-		foo = Image.open(file)  # My image is a 200x374 jpeg that is 102kb large
+		foo = Image.open(file)  # Gambar yg asalnya 200x374 jpeg dengan ukuran 102kb, gede cuy
 	 
 		if i == 'only':
-	 		foo.save(f"compress/{outname}/img_press.jpg", quality=95, optimize=True)  # The saved downsized image size is 24.8kb
+	 		foo.save(f"compress/{outname}/img_press.jpg", quality=95, optimize=True)  # Jadi 24.8kb
 		else:
 	 		foo.save(f"compress/{outname}/img_{i}_press.jpg", quality=70, optimize=True)
 	 	
